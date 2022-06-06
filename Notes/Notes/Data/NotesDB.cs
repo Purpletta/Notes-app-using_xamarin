ae69faee5be4b185bdcac75fc0e4aa02063c14ba
Purpletta <101121914+Purpletta@users.noreply.github.com>
2022-06-06 23:09:56 +0300

    Add files via upload

---
+++ Notes/Notes/Data/NotesDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Notes.Models;
+using System.Threading.Tasks;
+namespace Notes.Data
+{
+    public class NotesDB
+    {
+        readonly SQLiteAsyncConnection db;
+        public NotesDB(string connectionString)
+        {
+            db = new SQLiteAsyncConnection(connectionString);
+            db.CreateTableAsync<Note>().Wait();
+
+        }
+
+        public Task<List<Note>> GetNotesAsync() {
+        return db.Table<Note>().ToListAsync();
+        }
+        /*   public Task<List<Note>> GetNoteAsync(int id) {
+
+               return db.Table<Note>().Where(i => i.ID == id).FirstOrDefaultAsync();
+
+           }*/
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return db.Table<Note>().FirstOrDefaultAsync(x => x.ID == id);
+        }
+
+        public Task<int> SaveNoteAsync(Note note) {
+            if (note.ID != 0)
+            {
+                return db.UpdateAsync(note);
+            }
+            else 
+            {
+            
+                return db.InsertAsync(note);
+            
+            }
+        
+        }
+
+        public Task<int> DeleteNoteAsync(Note note) {
+        
+        return db.DeleteAsync(note);
+        
+        }
+
+    }
+}
---
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+   
+    public partial class NotesPage : ContentPage
+    {  // CollectionView collectionView = new CollectionView();
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            collectionView.ItemsSource = await App.NotesDB.GetNotesAsync(); 
+            base.OnAppearing();
+        }
+
+        private async void AddButton_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(NoteAddingPage));
+
+        }
+
+        private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        { if (e.CurrentSelection != null) {
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync(
+                    $"{nameof(NoteAddingPage)}?{nameof(NoteAddingPage.ItemID)}={note.ID.ToString()}");
+            }
+
+
+        }
+    }
+}
